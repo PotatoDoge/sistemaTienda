afda139510372b178fea870d7ad93c9ff5f3c1eb
bprado <bastian.prado.v@gmail.com>
2021-10-16 15:05:28 -0700

    - Ya registra productos en la bd (también compara si existen) - Falta ver como vaciar los campos una vez se registró el producto

--- app/src/main/java/com/example/sistematienda/AddProduct.kt
+++ app/src/main/java/com/example/sistematienda/AddProduct.kt
@@ -1,24 +1,162 @@
 package com.example.sistematienda
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class AddProduct : AppCompatActivity() {
+
+    private var registered: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
+
+        // Elementos
         val returnButton = findViewById<ImageButton>(R.id.returnAdd)
+        val agregarProd = findViewById<Button>(R.id.agregarProducto)
+        val nuevoProd = findViewById<EditText>(R.id.addClaveProducto)
+        val nuevaDescProd = findViewById<EditText>(R.id.addDescripcionProducto)
+        val nuevoNombrProd = findViewById<EditText>(R.id.addNombreProducto)
+
+
+        // Listeners
         returnButton.setOnClickListener{
             val intent = Intent(this, ProductsActivty::class.java)
             startActivity(intent)
             finish()
             this.overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right)
         }
+
+        agregarProd.setOnClickListener {
+            if(checkIfFieldsEmpty(nuevoProd,nuevaDescProd,nuevoNombrProd)){
+                val id = nuevoProd.text.toString()
+                val name = nuevoNombrProd.text.toString()
+                val desc = nuevaDescProd.text.toString()
+                checkAvailability(id, name,desc,"http://charlyffs.mywire.org:9000/checar_disponibilidad.php")
+            }
+            if(registered){
+                nuevoProd.text.clear()
+                nuevaDescProd.text.clear()
+                nuevoNombrProd.text.clear()
+                registered = false
+            }
+        }
     }
 
+    /**
+     * Blocks 'going back' gestures
+     */
     override fun onBackPressed() {
         moveTaskToBack(false)
+    }
+
+    /**
+     * Method that shows an alert with the required title and msg
+     */
+    private fun throwAlert(title:String, msg: String){
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage(msg)
+            // if the dialog is cancelable
+            .setCancelable(false)
+            // positive button
+            .setPositiveButton("Ok", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.dismiss()
+            })
+        val alert = dialogBuilder.create()
+        alert.setTitle(title)
+        alert.show()
+    }
+
+    /**
+     * Methd that checks if fields are empty
+     */
+    private fun checkIfFieldsEmpty(f1: EditText, f2:EditText, f3:EditText): Boolean{
+        if(f1.text.isBlank() || f2.text.isBlank() || f3.text.isBlank()){
+            throwAlert("Llenar campos", "Todos los campos tienen que estar llenos para seguir con el registro")
+            return false
+        }
+        return true
+    }
+
+    /**
+     * Method that checks if id exists in database. If it does not, it calls registerProductInDatabase method and handles products registration
+     */
+    private fun checkAvailability(id: String, name:String, dsc:String, URL: String){
+        val rq: RequestQueue = Volley.newRequestQueue(this)
+        val stringRequest = object : StringRequest(
+            Method.POST, URL, com.android.volley.Response.Listener { response ->
+                // lo que responde
+                if(response.isNotEmpty()){
+                    // encontró articulo con el mismo nombre
+                    throwAlert("Ya existe","Ya existe un prodcuto registrado con esa clave. Intente con otra de favor.")
+                }
+                else{
+                    registerProductInDatabase(id,name,dsc,"http://charlyffs.mywire.org:9000/agregar_producto.php")
+                    //registerProductInDatabase("B","B","B","http://charlyffs.mywire.org:9000/agregar_producto.php")
+                    Toast.makeText(this,"Producto registrado correctamente", Toast.LENGTH_SHORT).show()
+                    registered = true
+                }
+            },
+            {
+                // lo que pasa si hay error
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+            })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String?> {
+                val parametros = HashMap<String,String>()
+                parametros["idProd"] = id
+                return parametros
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                return HashMap()
+            }
+        }
+        // Add the request to the RequestQueue.
+        rq.add(stringRequest)
+    }
+
+    /**
+     * Method that registers products in the database
+     */
+    private fun registerProductInDatabase(id: String, name: String, desc: String, URL: String){
+        val rq: RequestQueue = Volley.newRequestQueue(this)
+        val stringRequest = object : StringRequest(
+            Method.POST, URL, com.android.volley.Response.Listener { _ -> },
+            {
+                // lo que pasa si hay error
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+            })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String?> {
+                val parametros = HashMap<String,String>()
+                parametros["id"] = id
+                parametros["nombre"] = name
+                parametros["desc"] = desc
+                return parametros
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                return HashMap()
+            }
+        }
+        // Add the request to the RequestQueue.
+        rq.add(stringRequest)
     }
 }
