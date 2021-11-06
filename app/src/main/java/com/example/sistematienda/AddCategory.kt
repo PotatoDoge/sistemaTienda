75f05965106ebacc35c3eca4effbcd2f69d7ab41
bprado <bastian.prado.v@gmail.com>
2021-11-06 13:56:27 -0700

    - Procesos relacionados a añadir categoría ya fueron implementados en el sistema

--- app/src/main/java/com/example/sistematienda/AddCategory.kt
+++ app/src/main/java/com/example/sistematienda/AddCategory.kt
@@ -9,6 +9,10 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class AddCategory : AppCompatActivity() {
 
@@ -46,10 +50,9 @@ class AddCategory : AppCompatActivity() {
 
         agregarCat.setOnClickListener{
             if(checkIfFieldsEmpty(nuevaCat, nuevaDescCat, nuevoNombrCat)){
-                // implementar método que inserta valores a la bd
+                checkAvailability(nuevaCat.text.toString(),nuevoNombrCat.text.toString(), nuevaDescCat.text.toString(), "http://charlyffs.mywire.org:9000/checar_disponibilidad_cat.php")
             }
         }
-
     }
 
     override fun onBackPressed() {
@@ -82,5 +85,76 @@ class AddCategory : AppCompatActivity() {
         val alert = dialogBuilder.create()
         alert.setTitle(title)
         alert.show()
+    }
+
+    /**
+     * Method that checks if id exists in database. If it does not, it calls registerCategoryInDatabase method and handles products registration
+     */
+    private fun checkAvailability(id: String, name:String, dsc:String, URL: String){
+        val rq: RequestQueue = Volley.newRequestQueue(this)
+        val stringRequest = object : StringRequest(
+            Method.POST, URL, com.android.volley.Response.Listener { response ->
+                // lo que responde
+                if(response.isNotEmpty()){
+                    // encontró articulo con el mismo nombre
+                    throwAlert("Ya existe","Ya existe una categoría registrada con esa clave. Intente con otra de favor.")
+                }
+                else{
+                    registerCategoryInDatabase(id,name,dsc,"http://charlyffs.mywire.org:9000/agregar_categoria.php")
+                    Toast.makeText(this,"Categoría registrada correctamente", Toast.LENGTH_SHORT).show()
+                    nuevaCat.text.clear()
+                    nuevaDescCat.text.clear()
+                    nuevoNombrCat.text.clear()
+                }
+            },
+            {
+                // lo que pasa si hay error
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+            })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String?> {
+                val parametros = HashMap<String,String>()
+                parametros["idCat"] = id
+                return parametros
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                return HashMap()
+            }
+        }
+        // Add the request to the RequestQueue.
+        rq.add(stringRequest)
+    }
+
+    /**
+     * Method that registers categories in the database
+     */
+    private fun registerCategoryInDatabase(id: String, name: String, desc: String, URL: String){
+        val rq: RequestQueue = Volley.newRequestQueue(this)
+        val stringRequest = object : StringRequest(
+            Method.POST, URL, com.android.volley.Response.Listener { _ -> },
+            {
+                // lo que pasa si hay error
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+            })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String?> {
+                val parametros = HashMap<String,String>()
+                parametros["id"] = id
+                parametros["nombre"] = name
+                parametros["desc"] = desc
+                return parametros
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                return HashMap()
+            }
+        }
+        // Add the request to the RequestQueue.
+        rq.add(stringRequest)
     }
 }
